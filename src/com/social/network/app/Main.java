5fb9f7a9f5653aec095bf3ecd9b3ece1d00b84b3
tlima1011 <thiagolima1011@gmail.com>
2026-04-20 15:54:14 -0300

    Projeto criado Java Social Network

---
+++ src/com/social/network/app/Main.java
@@ -0,0 +1,10 @@
+package com.social.network.app;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Bem vindo novo projeto java social network");
+	}
+
+}
