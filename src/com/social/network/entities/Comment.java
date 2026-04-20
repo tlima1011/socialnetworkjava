8c66c738ff8eb6c6c41ffcf9906cecdad9eede57
tlima1011 <thiagolima1011@gmail.com>
2026-04-20 15:57:34 -0300

    Classe Comment Criada

---
+++ src/com/social/network/entities/Comment.java
@@ -0,0 +1,16 @@
+package com.social.network.entities;
+
+public class Comment {
+	
+	private String Text;
+	
+	public Comment() {
+		
+	}
+
+	public Comment(String text) {
+		Text = text;
+	}
+	
+	
+}
